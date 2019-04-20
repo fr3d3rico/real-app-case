7161e5b919c446c8689f7ee71de9abefa2848de5
Fred <frederico.a.soares@gmail.com>
2019-04-20 11:29:58 -0300

    Added info gitigonre. Added backend initial commit.

---
+++ backend-real-app-case/src/main/java/com/github/fr3d3rico/BackendRealAppCaseApplication.java
@@ -0,0 +1,13 @@
+package com.github.fr3d3rico;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendRealAppCaseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendRealAppCaseApplication.class, args);
+	}
+
+}
