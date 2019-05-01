a7d5fdf14e8ed0bdd5af27bd4cb9635b5629f18a
Fred <frederico.a.soares@gmail.com>
2019-05-01 12:58:50 -0300

    Added tests cases.

--- backend-real-app-case/src/test/java/com/github/fr3d3rico/backendrealappcase/controller/AuthControllerTest.java
+++ backend-real-app-case/src/test/java/com/github/fr3d3rico/backendrealappcase/controller/AuthControllerTest.java
@@ -1,7 +1,11 @@
 package com.github.fr3d3rico.backendrealappcase.controller;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import java.util.HashMap;
+import java.util.Map;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -12,6 +16,7 @@ import org.springframework.http.MediaType;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.github.fr3d3rico.backendrealappcase.model.JwtTokenProvider;
 import com.github.fr3d3rico.backendrealappcase.repository.RoleRepository;
 import com.github.fr3d3rico.backendrealappcase.repository.UserRepository;
@@ -20,6 +25,9 @@ import com.github.fr3d3rico.backendrealappcase.services.CustomUserDetailsService
 @RunWith(SpringRunner.class)
 @WebMvcTest
 public class AuthControllerTest {
+	
+	private final String LOGIN_URL = "/api/auth/login";
+	private final String REGISTER_URL = "/api/auth/register";
 	
 	@Autowired
     private MockMvc mvc;
@@ -37,11 +45,55 @@ public class AuthControllerTest {
 	private CustomUserDetailsService userService;
 
     @Test
-    public void testLogin() throws Exception {
-    	mvc.perform(get("/api/auth/login")
+    public void testWrongLoginGETMethod() throws Exception {
+    	mvc.perform(get(LOGIN_URL)
     		      .contentType(MediaType.APPLICATION_JSON))
 				  .andExpect(status().is(405));
     		      //.andExpect(status().isOk());
     }
+    
+    @Test
+    public void testWrongLoginPOSTMethodEmptyData() throws Exception {
+    	Map<Object, Object> model = new HashMap<>();
+    	
+    	mvc.perform(post(LOGIN_URL)
+    			.content(new ObjectMapper().writeValueAsString(model))
+    		    .contentType(MediaType.APPLICATION_JSON))
+				.andExpect(status().is(401));
+    		    //.andExpect(status().isOk());
+    }
+    
+    @Test
+    public void testWrongLoginPOSTMethod() throws Exception {
+    	Map<Object, Object> model = new HashMap<>();
+    	
+    	model.put("email", "ppp@fred.com");
+    	model.put("password", "123456");
+    	
+    	mvc.perform(post(LOGIN_URL)
+    			.content(new ObjectMapper().writeValueAsString(model))
+    		    .contentType(MediaType.APPLICATION_JSON))
+				.andExpect(status().is(401));
+    		    //.andExpect(status().isOk());
+    }
+    
+    @Test
+    public void testRegisterPOSTMethod() throws Exception {
+    	Map<Object, Object> model = new HashMap<>();
+    	
+    	model.put("email", "frederico2@fred.com");
+    	model.put("password", "123456");
+    	
+    	mvc.perform(post(REGISTER_URL)
+    			.content(new ObjectMapper().writeValueAsString(model))
+    		    .contentType(MediaType.APPLICATION_JSON))
+    		    .andExpect(status().isOk());
+    }
 	
+    @Test
+    public void testWrongRegisterGETMethod() throws Exception {
+    	mvc.perform(get(REGISTER_URL)
+    			.contentType(MediaType.APPLICATION_JSON))
+    			.andExpect(status().is(405));
+    }
 }
